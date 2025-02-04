72c26c6ac4163cd28617abff3cb14c3a6b7fdedd
RobertoPanczi <panczi.roberto@yahoo.com>
2025-02-04 10:03:07 +0100

    inizio-libri

--- Libri.java
+++ Libri.java
@@ -1,3 +1,23 @@
 public class Libri {
+    protected String autore;
+    protected String nome;
+    protected String genere;
+    protected int pagine;
+    protected int[] dataI;
+    protected int[] dataF;
+    protected String trama;
+    //attributo per gli stati
+    //attributo per il punteggio(recensione)
+    public Libri(String autore, String nome, String genere, int pagine, int[] dataI, int[] dataF, String trama) {
+        this.autore = autore;
+        this.nome = nome;
+        this.genere = genere;
+        this.pagine = pagine;
+        this.dataI = dataI;
+        this.dataF = dataF;
+        this.trama = trama;
+    }
+
+    
     
 }

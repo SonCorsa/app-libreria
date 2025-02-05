d4b974517765b501cfb7d2739985132d1954d54b
SonCorsa <lupin2007.fc@gmail.com>
2025-02-05 08:52:45 +0100

    messaggino per Roberto <3

---
+++ Finestra.java
@@ -0,0 +1,8 @@
+public class Finestra {
+    //ciao roberto mi manchi mentre stai spiegando alla lavagna.
+    //ricordati di NON toccare nessuno degli altri file,ho mi fotti il lavoro.
+    //ho già creato la branch, lavorato sopra, fatto il merge ed eliminato la branch perciò qualsiasi cosa fai lavora SOLO su questo file.
+    //i test li facciamo quando facciamo il merge
+    //muah :3
+    
+}

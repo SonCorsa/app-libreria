4171a2f866dbf889cdd0d4f97ed26fa704978791
RobertoPanczi <panczi.roberto@yahoo.com>
2025-02-05 10:02:34 +0100

    +Finestra+Home

--- Home.java
+++ Home.java
@@ -1,8 +1,14 @@
-public class Finestra {
+import javax.swing.*;
+import javax.awt.*;
+
+public class Home extends JFrame{
+    private JLabel immagine;
+    private 
     //ciao roberto mi manchi mentre stai spiegando alla lavagna.
     //ricordati di NON toccare nessuno degli altri file,ho mi fotti il lavoro.
     //ho già creato la branch, lavorato sopra, fatto il merge ed eliminato la branch perciò qualsiasi cosa fai lavora SOLO su questo file.
     //i test li facciamo quando facciamo il merge
     //muah :3
+    
     
 }

d4bd8faa8e5168bb30749491fff099677def9236
SonCorsa <lupin2007.fc@gmail.com>
2025-02-06 11:28:46 +0100

    comit per tornare indietro

--- Home.java
+++ Home.java
@@ -1,20 +1,20 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Home extends JFrame{
+public class Home{
     //ciao roberto mi manchi mentre stai spiegando alla lavagna.
     //ricordati di NON toccare nessuno degli altri file,ho mi fotti il lavoro.
     //ho già creato la branch, lavorato sopra, fatto il merge ed eliminato la branch perciò qualsiasi cosa fai lavora SOLO su questo file.
     //i test li facciamo quando facciamo il merge
     //muah :3
-    
+    private JFrame homePage;
     private JButton profileButton; 
     private JLabel label; 
     private JPanel panel;
     
     public Home(String titolo){
-        super(titolo);
-        setLayout(new BorderLayout());
+        homePage= new JFrame(titolo);
+        homePage.setLayout(new BoxLayout(homePage,BoxLayout.Y_AXIS));
         
         profileButton = new JButton("profilo");
         label = new JLabel("Reading");
@@ -22,14 +22,20 @@ public class Home extends JFrame{
 
         panel.add(profileButton,BorderLayout.EAST);
         panel.add(label,BorderLayout.WEST);
-        add(panel,BorderLayout.NORTH);
+        homePage.add(panel);
+        
 
-        setSize(300,200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        homePage.setSize(300,200);
+        homePage.setLocationRelativeTo(null);
+        homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void setVisible(boolean b){
+        homePage.setVisible(b);
     }
 
     /*ToDo:
+     * sistemare il BoxLayout
      * mettere le altre 3 righe
      * inserire l'immagine nel JButton
      * impostare il JButton come un cerchio

0b8402b6c7cb71991ceaa936a20b84b782d5d746
SonCorsa <lupin2007.fc@gmail.com>
2025-02-06 11:34:09 +0100

    ritorno punto e a capo (COME CAZZO FUNZIONA IL BOXLAYOUT)

--- Home.java
+++ Home.java
@@ -1,20 +1,20 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Home{
+public class Home extends  JFrame{
     //ciao roberto mi manchi mentre stai spiegando alla lavagna.
     //ricordati di NON toccare nessuno degli altri file,ho mi fotti il lavoro.
     //ho già creato la branch, lavorato sopra, fatto il merge ed eliminato la branch perciò qualsiasi cosa fai lavora SOLO su questo file.
     //i test li facciamo quando facciamo il merge
     //muah :3
-    private JFrame homePage;
+
     private JButton profileButton; 
     private JLabel label; 
     private JPanel panel;
     
     public Home(String titolo){
-        homePage= new JFrame(titolo);
-        homePage.setLayout(new BoxLayout(homePage,BoxLayout.Y_AXIS));
+        super(titolo);
+        setLayout(new BorderLayout());
         
         profileButton = new JButton("profilo");
         label = new JLabel("Reading");
@@ -22,16 +22,13 @@ public class Home{
 
         panel.add(profileButton,BorderLayout.EAST);
         panel.add(label,BorderLayout.WEST);
-        homePage.add(panel);
+        add(panel,BorderLayout.NORTH);
         
 
-        homePage.setSize(300,200);
-        homePage.setLocationRelativeTo(null);
-        homePage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public void setVisible(boolean b){
-        homePage.setVisible(b);
+        setSize(300,200);
+    
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     /*ToDo:

5b880d9c907b42cace1a7ae4837f92da1b4812e5
SonCorsa <lupin2007.fc@gmail.com>
2025-02-06 10:58:34 +0100

    Prova della home

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 public class App{
     public static void main(String args[]){
+        Home home= new Home("prova");
+        home.setVisible(true);
 
     }
 }
--- Home.java
+++ Home.java
@@ -1,14 +1,32 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.*;
-import javax.awt.*;
 
 public class Home extends JFrame{
-    private JLabel immagine;
-    private 
     //ciao roberto mi manchi mentre stai spiegando alla lavagna.
     //ricordati di NON toccare nessuno degli altri file,ho mi fotti il lavoro.
     //ho già creato la branch, lavorato sopra, fatto il merge ed eliminato la branch perciò qualsiasi cosa fai lavora SOLO su questo file.
     //i test li facciamo quando facciamo il merge
     //muah :3
     
+    private JButton profileButton; 
+    private JTextPane tp; 
+    private JPanel panel;
+    
+    public Home(String titolo){
+        super(titolo);
+        setLayout(new BorderLayout());
+        
+        profileButton = new JButton("pulsante del profilo");
+        tp=new JTextPane();
+        panel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        panel.add(profileButton);
+        add(panel,BorderLayout.NORTH);
+
+        setSize(300,200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     
 }

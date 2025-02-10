fa822ef42ef01ee92f2e775ab687f500ef74a37c
SonCorsa <lupin2007.fc@gmail.com>
2025-02-10 11:15:10 +0100

    NONVAPORCODIO

--- Home.java
+++ Home.java
@@ -24,8 +24,8 @@ public class Home extends  JFrame implements ActionListener{
         super(titolo);
         setLayout(new BorderLayout());
         //IMAGINE PROFILO
-        fileicon = new File("user.png");  //se stai leggendo questo messaggio, significa che ho trovato la fonte del problema: in windows il path si scrive con il backslash, non con lo slash e per intero. PORCODIO
-        icon = new ImageIcon(fileicon.getAbsolutePath());
+        fileicon = new File("esempi utili/user.png");  //se stai leggendo questo messaggio, significa che ho trovato la fonte del problema: in windows il path si scrive con il backslash, non con lo slash e per intero. PORCODIO
+        icon = new ImageIcon(fileicon.getAbsolutePath());  //!!PORCAMADONNA
         
 
         //istanzio JButton

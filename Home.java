205c873ebffe40fadfb868f93ccb5ab3ef928bd6
SonCorsa <lupin2007.fc@gmail.com>
2025-02-13 21:07:50 +0100

    puercoiddio

--- Home.java
+++ Home.java
@@ -4,23 +4,55 @@ import java.io.*;
 import java.util.ArrayList;
 import javax.swing.*;
 
-public class Home extends  JFrame implements ActionListener{
+public class Home{
     //ciao roberto mi manchi mentre stai spiegando alla lavagna.
     //ricordati di NON toccare nessuno degli altri file,ho mi fotti il lavoro.
     //ho già creato la branch, lavorato sopra, fatto il merge ed eliminato la branch perciò qualsiasi cosa fai lavora SOLO su questo file.
     //i test li facciamo quando facciamo il merge
     //muah :3
-
+    
+    private JFrame frame;
     private JButton profileButton,aggiungiRead,aggiungiReading,aggiungiToRead,indietro; 
     private final JLabel home,reading,read,toRead; 
     private final JPanel p1,p2,readPanel,readingPanel,toReadPanel,aggiungiPanel;
-    private final ArrayList<JPanel> readList;
-    private final ArrayList<JPanel> readingList,toReadList;
     private ImageIcon icon;
     private final Aggiungi addPage;
     private File fileicon;
-    
+
     public Home(String titolo){
+        //istanzio il frame
+        frame = new JFrame(titolo);
+        
+        //istanzio i JButton
+        profileButton = new JButton();
+        aggiungiRead = new JButton("+");
+        aggiungiReading= new JButton("+");
+        aggiungiToRead = new JButton("+");
+        indietro=new JButton("indietro"); 
+        
+        //istanzio i JLabel
+        home = new JLabel("    home");
+        read= new JLabel("Read");
+        reading = new JLabel("Reading");
+        toRead= new JLabel("To Read");
+        
+        
+        //istanzio i JPanel
+        p1 = new JPanel(new BorderLayout());
+        p2=new JPanel(new GridLayout(6,0));
+        aggiungiPanel = new JPanel(new FlowLayout(FlowLayout.LEFT)); //!!da mettere in Aggiungi
+        readPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 10));
+        readingPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 10));
+        toReadPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 20, 10));
+        
+        //istanzio Aggiungi
+        addPage=new Aggiungi("aggiungi un libro");  //!!da gestire con controller
+        /*fileicon = new File("app-libreria/user.png");
+        icon = new ImageIcon(fileicon.getAbsolutePath());*/
+
+    }
+    
+    /*public Home(String titolo){
         super(titolo);
         setLayout(new BorderLayout());
         //IMAGINE PROFILO
@@ -133,7 +165,7 @@ public class Home extends  JFrame implements ActionListener{
             this.setVisible(true);
             addPage.setVisible(false);
         }
-    }
+    }*/
 
     /*ToDo:
      * creare il pulsante per visualizzare la lista
